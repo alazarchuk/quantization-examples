a65817c75b0ad87bcaa9c08123b93db4f81a3ab3
Andrii Lazarchuk <alazarchuk@gmail.com>
2024-10-09 16:58:59 +0000

    First reference examples

---
+++ bnb-4bit/inference-Mistral-7B-Instruct-v0.3-f16.py
@@ -0,0 +1,34 @@
+import torch
+import wandb
+import os
+from dotenv import load_dotenv
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+load_dotenv()
+
+# Check if the current CUDA device supports bfloat16 precision
+if torch.cuda.is_bf16_supported():
+    compute_dtype = torch.bfloat16
+else:
+    compute_dtype = torch.float16
+
+# Define the model name and the path where the quantized model will be saved
+quant_path = "Mistral-7B-Instruct-v0.3-bnb-4bit"
+model_name = "../../Models/" + quant_path
+
+run = wandb.init(
+    project=f'{os.environ["WANDB_PROJECT_PREFIX"]} -> {quant_path} (F16)',
+    job_type="inference",
+)
+
+# Load the tokenizer for the specified model
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Load the model with the specified quantization configuration
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=compute_dtype)
+
+input_text = "Write me a story about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids, max_new_tokens=1024)
+print(tokenizer.decode(outputs[0]))
---
+++ reference/inference-Mistral-7B-Instruct-v0.3.py
@@ -0,0 +1,28 @@
+import torch
+import wandb
+import os
+from dotenv import load_dotenv
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+load_dotenv()
+
+model_name = "mistralai/Mistral-7B-Instruct-v0.3"
+
+run = wandb.init(
+    project=f'{os.environ["WANDB_PROJECT_PREFIX"]} -> {model_name.split("/")[1]}',
+    job_type="inference",
+)
+
+# Load the tokenizer for the specified model
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Load the model with the specified quantization configuration
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, device_map="cuda", torch_dtype=torch.float16
+)
+
+input_text = "Write me a story about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids, max_new_tokens=1024)
+print(tokenizer.decode(outputs[0]))
